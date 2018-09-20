12e58b34701ef1632858be5cb74f8f4b569c9a1b
Shahrukh <jalilshahrukh@gmail.com>
2018-09-20 14:15:31 -0400

    Some structure.

---
+++ components/app.jsx
@@ -0,0 +1,13 @@
+/*eslint-disable*/
+
+import React, { Component } from 'react';
+import { render } from 'react-dom'; 
+
+class App extends Component { 
+  constructor() { 
+    super(); 
+    this.state = {};
+  }
+}
+
+render(<App />, document.getElementById('content'));
